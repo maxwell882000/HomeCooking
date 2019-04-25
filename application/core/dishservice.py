ef6d6bd7dc90d7659d4dc513fbce359048a03945
dmitriyl99 <dmitriyl1899@gmail.com>
2019-04-26 03:18:04 +0500

    Search dishes and categories by name with language

--- application/core/dishservice.py
+++ application/core/dishservice.py
@@ -6,13 +6,19 @@ def get_all_categories() -> DishCategory:
     return DishCategory.query.all()
 
 
-def get_dishes_by_category_name(name: str) -> list:
-    dish_category = DishCategory.query.filter(DishCategory.name == name).get()
+def get_dishes_by_category_name(name: str, language: str) -> list:
+    if language == 'uz':
+        dish_category = DishCategory.query.filter(DishCategory.name_uz == name).get()
+    else:
+        dish_category = DishCategory.query.filter(DishCategory.name == name).get()
     if dish_category:
         return dish_category.dishes.all()
     return []
 
 
-def get_dish_by_name(name: str) -> Dish:
-    dish = Dish.query.filter(Dish.name == name).get()
+def get_dish_by_name(name: str, language: str) -> Dish:
+    if language == 'uz':
+        dish = Dish.query.filter(Dish.name_uz == name).get()
+    else:
+        dish = Dish.query.filter(Dish.name == name).get()
     return dish
