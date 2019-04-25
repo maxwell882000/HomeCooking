d4c7f2e07af47fe0b3ced01a5242c81755c5d089
dmitriyl99 <dmitriyl1899@gmail.com>
2019-04-25 17:01:47 +0500

    Add carts and order models and relationships for them

--- application/core/models.py
+++ application/core/models.py
@@ -3,12 +3,24 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
+cart_items = db.Table('cart_items',
+                      db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                      db.Column('dish_id', db.Integer, db.ForeignKey('dish.id'), primary_key=True),
+                      db.Column('count', db.Integer))
+
+order_items = db.Table('order_items',
+                       db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+                       db.Column('dish_id', db.Integer, db.ForeignKey('dish.id'), primary_key=True),
+                       db.Column('count', db.Integer))
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     username = db.Column(db.String(100))
     phone_number = db.Column(db.String(15))
     language = db.Column(db.String(5))
+    cart = db.relationship('Dish', secondary=cart_items, lazy='dynamic')
 
 
 class UserAdmin(db.Model, UserMixin):
@@ -37,6 +49,18 @@ class Dish(db.Model):
     description = db.Column(db.String(500))
     price = db.Column(db.Integer)
     category_id = db.Column(db.Integer, db.ForeignKey('dish_categories.id'))
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    type = db.Column(db.Integer)
+    order_items = db.relationship('Dish', secondary=order_items, lazy='dynamic',
+                                  backref=db.backref('orders', lazy=True))
+
+    class OrderTypes:
+        PICK_UP = 1
+        DELIVERY = 2
 
 
 @login.user_loader
