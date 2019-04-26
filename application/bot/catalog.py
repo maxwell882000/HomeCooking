457040bb8f43214b8e02fbc5034e38b0d6b39205
dmitriyl99 <dmitriyl1899@gmail.com>
2019-04-26 17:29:04 +0500

    Catalog processors, show categories, dishes and add dish to user cart

--- application/bot/catalog.py
+++ application/bot/catalog.py
@@ -3,6 +3,7 @@ from application.core import userservice, dishservice
 from application.resources import strings, keyboards
 from telebot.types import Message
 from application.core import exceptions
+from . import cart
 
 
 def check_catalog(message: Message):
@@ -27,8 +28,79 @@ def back_to_the_catalog(chat_id, language, message_txt=None):
     bot.register_next_step_handler_by_chat_id(chat_id, catalog_processor)
 
 
+def dish_action_processor(message: Message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    language = userservice.get_user_language(user_id)
+
+    def error():
+        error_message = strings.get_string('catalog.dish_action_error', language)
+        bot.send_message(chat_id, error_message)
+        bot.register_next_step_handler_by_chat_id(chat_id, dish_action_processor)
+
+    if not message.text:
+        error()
+        return
+    current_dish = userservice.get_current_user_dish(user_id)
+    if strings.get_string('go_back', language) in message.text:
+        dishes = current_dish.category.dishes.all()
+        dish_message = strings.get_string('catalog.choose_dish', language)
+        dishes_keyboard = keyboards.from_dishes(dishes, language)
+        bot.send_message(chat_id, dish_message, reply_markup=dishes_keyboard)
+        bot.register_next_step_handler_by_chat_id(chat_id, choose_dish_processor)
+    elif strings.get_string('catalog.cart', language) in message.text:
+        cart.cart_processor(message)
+    else:
+        if not message.text.isdigit():
+            error()
+            return
+        userservice.add_dish_to_cart(user_id, current_dish, int(message.text))
+        continue_message = strings.get_string('catalog.continue', language)
+        back_to_the_catalog(chat_id, language, continue_message)
+
+
 def choose_dish_processor(message: Message):
-    pass
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    language = userservice.get_user_language(user_id)
+
+    def error():
+        error_message = strings.get_string('catalog.dish_error', language)
+        bot.send_message(chat_id, error_message)
+        bot.register_next_step_handler_by_chat_id(chat_id, choose_dish_processor)
+
+    if not message.text:
+        error()
+        return
+    if strings.get_string('go_back', language) in message.text:
+        back_to_the_catalog(chat_id, language)
+    elif strings.get_string('catalog.cart', language) in message.text:
+        cart.cart_processor(message)
+    else:
+        dish_name = message.text
+        dish = dishservice.get_dish_by_name(dish_name, language)
+        if not dish:
+            error()
+            return
+        userservice.set_current_user_dish(user_id, dish.id)
+        dish_info = strings.from_dish(dish, language)
+        dish_keyboard = keyboards.get_keyboard('catalog.dish_keyboard', language)
+        if dish.image_id or dish.image_path:
+            if dish.image_path and not dish.image_id:
+                try:
+                    image = open(dish.image_path, 'r')
+                except FileNotFoundError:
+                    bot.send_message(chat_id, dish_info, reply_markup=dish_keyboard)
+                else:
+                    sent_message = bot.send_photo(chat_id, image, caption=dish_info, reply_markup=dish_keyboard)
+                    dishservice.set_dish_image_id(dish, sent_message.photo[-1].file_id)
+            elif dish.image_id:
+                bot.send_photo(chat_id, dish.image_id, caption=dish_info, reply_markup=dish_keyboard)
+        else:
+            bot.send_message(chat_id, dish_info, reply_markup=dish_keyboard)
+        dish_action_helper = strings.get_string('catalog.dish_action_helper', language)
+        bot.send_message(chat_id, dish_action_helper)
+        bot.register_next_step_handler_by_chat_id(chat_id, dish_action_processor)
 
 
 def catalog_processor(message: Message):
@@ -46,16 +118,22 @@ def catalog_processor(message: Message):
         return
     if strings.get_string('go_back', language) in message.text:
         back_to_the_catalog(chat_id, language)
-        return
-    try:
-        dishes = dishservice.get_dishes_by_category_name(message.text, language)
-    except exceptions.CategoryNotFoundError:
-        error()
-        return
-    dish_message = strings.get_string('catalog.choose_dish', language)
-    dishes_keyboard = keyboards.from_dishes(dishes, language)
-    bot.send_message(chat_id, dish_message, reply_markup=dishes_keyboard)
-    bot.register_next_step_handler_by_chat_id(chat_id, choose_dish_processor)
+    elif strings.get_string('catalog.cart', language) in message.text:
+        cart.cart_processor(message)
+    elif strings.get_string('catalog.make_order', language) in message.text:
+        # TODO: Redirect to order processor
+        pass
+    else:
+        try:
+            category_name = message.text
+            dishes = dishservice.get_dishes_by_category_name(category_name, language)
+        except exceptions.CategoryNotFoundError:
+            error()
+            return
+        dish_message = strings.get_string('catalog.choose_dish', language)
+        dishes_keyboard = keyboards.from_dishes(dishes, language)
+        bot.send_message(chat_id, dish_message, reply_markup=dishes_keyboard)
+        bot.register_next_step_handler_by_chat_id(chat_id, choose_dish_processor)
 
 
 @bot.message_handler(commands=['order'], func=check_auth)
