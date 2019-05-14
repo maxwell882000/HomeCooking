4d6929bb373630adaf24a1551bac5b21c3c0dc58
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-14 12:26:29 +0500

    Delete name field from user

--- application/core/models.py
+++ application/core/models.py
@@ -31,7 +31,6 @@ class User(db.Model):
     """
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
     username = db.Column(db.String(100))
     phone_number = db.Column(db.String(15))
     language = db.Column(db.String(5))
