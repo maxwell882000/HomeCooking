2302dbf90ff4cfc1eec004d259571f067afd4871
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-16 00:56:58 +0500

    Order phone number keyboard

--- application/resources/keyboards.py
+++ application/resources/keyboards.py
@@ -153,3 +153,13 @@ def from_change_language(current_language: str) -> ReplyKeyboardMarkup:
         change_language_keyboard.add(get_string('language.uzbek'))
     change_language_keyboard.add(get_string('go_back', current_language))
     return change_language_keyboard
+
+
+def from_user_phone_number(language, phone_number: str = None) -> ReplyKeyboardMarkup:
+    phone_number_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    if phone_number and phone_number != '':
+        phone_number_keyboard.add(phone_number)
+    phone_button = KeyboardButton(get_string('my_number', language), request_contact=True)
+    phone_number_keyboard.add(phone_button)
+    phone_number_keyboard.add(get_string('go_back', language))
+    return phone_number_keyboard
