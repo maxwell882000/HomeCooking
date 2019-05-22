f20b9b78780518f82a7e4cb2440f2a8a867328f0
dmitriyl99 <dmitriyl1899@gmail.com>
2019-05-22 17:48:28 +0500

    CRUD operations for categories

--- application/core/dishservice.py
+++ application/core/dishservice.py
@@ -1,10 +1,37 @@
 from application import db
 from application.core.models import Dish, DishCategory
 from application.core import exceptions
+from typing import List
 
 
-def get_all_categories() -> DishCategory:
+def get_all_categories() -> List[DishCategory]:
     return DishCategory.query.all()
+
+
+def get_category_by_id(category_id) -> DishCategory:
+    return DishCategory.query.get_or_404(category_id)
+
+
+def update_category(category_id: int, name_ru: str, name_uz:str):
+    category = DishCategory.query.get_or_404(category_id)
+    category.name = name_ru
+    category.name_uz = name_uz
+    db.session.commit()
+    return category
+
+
+def category_exists(category_name: str) -> bool:
+    return DishCategory.query.filter(DishCategory.name == category_name or DishCategory.name_uz == category_name).count() > 0
+
+
+def create_category(name_ru: str, name_uz: str):
+    category = DishCategory(name=name_ru, name_uz=name_uz)
+    db.session.add(category)
+    db.session.commit()
+
+
+def remove_category(category_id: int):
+    db.session.delete(DishCategory.query.get_or_404(category_id))
 
 
 def get_dishes_by_category_name(name: str, language: str) -> list:
