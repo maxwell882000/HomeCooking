c86f212ed49bf82fab698bd61e89dc9e5a5f036c
dmitriyl99 <dmitriyl1899@gmail.com>
2019-06-30 23:48:43 +0500

    Trello API keys and tokens

--- config.py
+++ config.py
@@ -17,3 +17,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    TRELLO_API_KEY = os.environ.get('TRELLO_API_KEY')
+    TRELLO_TOKEN = os.getenv('TRELLO_TOKEN')
